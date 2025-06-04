e8705315743d601508143f4747a6fb6e4d5cf1e1
Lutopia <lutopia@hotmail.fr>
2025-06-05 00:12:23 +0200

    server: track_get returns a shallow track (without buffer)
    This was causing disconnections

--- server/src/js/msg/track_get.js
+++ server/src/js/msg/track_get.js
@@ -6,9 +6,11 @@ export default {
   on_message: async (server, conn, msg) => {
     const existing_track = server.get_track(msg.track_hash)
     if(existing_track) {
+      const shallow_track = {...existing_track};
+      delete shallow_track.buffer;
       await msg.reply({
         status: 'success',
-        track: existing_track,
+        track: shallow_track,
       });
     } else {
       await msg.reply({
