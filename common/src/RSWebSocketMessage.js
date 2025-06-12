eb2f1fb959d2859f6f386e9e8072130f287b8867
Lutopia <lutopia@hotmail.fr>
2025-06-12 22:37:52 +0200

    common: WSMsg supports buffers recursively

--- common/src/RSWebSocketMessage.js
+++ common/src/RSWebSocketMessage.js
@@ -4,15 +4,20 @@ export default class RSWebSocketMessage {
   static from_object(obj) {
     let buffer_size = 4; // 4 first bytes for the size of the obj buffer
     let raw_buffers = [];
-    for(let key in obj) {
-      if(obj[key] instanceof Uint8Array) {
-        const raw_buffer_index = raw_buffers.length;
-        const raw_buffer = obj[key];
-        raw_buffers.push(raw_buffer);
-        obj[key] = `RAW_BUFFER_${raw_buffer_index}_${raw_buffer.byteLength}`;
-        buffer_size += raw_buffer.byteLength;
+
+    (function mark_buffers(obj){
+      for(let key in obj) {
+        if(obj[key] instanceof Uint8Array) {
+          const raw_buffer_index = raw_buffers.length;
+          const raw_buffer = obj[key];
+          raw_buffers.push(raw_buffer);
+          obj[key] = `RAW_BUFFER_${raw_buffer_index}_${raw_buffer.byteLength}`;
+          buffer_size += raw_buffer.byteLength;
+        } else if(Object.prototype == Object.getPrototypeOf(obj[key])) {
+          mark_buffers(obj[key]);
+        }
       }
-    }
+    })(obj);
 
     const obj_buffer = this.encoder.encode(JSON.stringify(obj));
     buffer_size += obj_buffer.byteLength;
@@ -42,23 +47,29 @@ export default class RSWebSocketMessage {
     const obj_array = new Uint8Array(array.buffer, array.byteOffset + 4, obj_buffer_size);
     const obj_str = this.decoder.decode(obj_array);
     const obj = JSON.parse(obj_str);
-
     let raw_buffer_offset = obj_buffer_size + 4;
-    for(let key in obj) {
-      const value = obj[key];
-      if(!(typeof value === 'string' || value instanceof String)) {
-        continue;
-      }
-      const match = value.match(/^RAW_BUFFER_(\d+)_(\d+)$/);
-      if(!match) {
-        continue;
-      }
-      const raw_buffer_index = parseInt(match[1]);
-      const raw_buffer_size = parseInt(match[2]);
 
-      obj[key] = new Uint8Array(array.buffer, array.byteOffset + raw_buffer_offset, raw_buffer_size);
-      raw_buffer_offset += raw_buffer_size;
-    }
+    (function fill_buffers(obj) {
+      for(let key in obj) {
+        const value = obj[key];
+        if(Object.prototype == Object.getPrototypeOf(obj[key])) {
+          fill_buffers(obj[key]);
+          continue;
+        }
+        if(!(typeof value === 'string' || value instanceof String)) {
+          continue;
+        }
+        const match = value.match(/^RAW_BUFFER_(\d+)_(\d+)$/);
+        if(!match) {
+          continue;
+        }
+        const raw_buffer_index = parseInt(match[1]);
+        const raw_buffer_size = parseInt(match[2]);
+
+        obj[key] = new Uint8Array(array.buffer, array.byteOffset + raw_buffer_offset, raw_buffer_size);
+        raw_buffer_offset += raw_buffer_size;
+      }
+    })(obj);
 
     return obj;
   }
