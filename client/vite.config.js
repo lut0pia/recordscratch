1f01ae72c34773f4bdf11da766947d9a9a71604e
Lou Catonnet <lou.catonnet@dont-nod.com>
2025-07-02 22:50:33 +0200

    client: Merged html for electron renderer and web

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,6 +7,10 @@ export default defineConfig(({command, mode}) => ({
   define: {
     __WEB__: JSON.stringify(true),
   },
+  root: './src/renderer',
+  build: {
+    outDir: '../../dist',
+  },
   plugins: [
     nodePolyfills(),
     vue(),
